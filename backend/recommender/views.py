25fea98ff8aef66418bea2ecec56b30771eaf741
Cory Fitz <coryalanfitz@gmail.com>
2023-05-08 14:21:56 +0100

    chat gpt initial commit

--- backend/recommender/views.py
+++ backend/recommender/views.py
@@ -1,14 +1,25 @@
 from rest_framework import views
 from rest_framework.response import Response
+import openai
 
 from .serializers import RecommenderSerializer
 
 class RecommenderView(views.APIView):
 
     def get_response(self):
-        return {'response': 'this is a sample chat gpt response'}
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": "Please recommend a movie for me to watch. I prefer serious movies, I prefer thinking about the future, and I'm ok watching movies with subtitles"}
+                ]
+        )
+
+        return {'response': response['choices'][0]['message']['content']}
 
     def get(self, request):
         yourdata= self.get_response()
         results = RecommenderSerializer(yourdata).data
         return Response(results)
+    
+    
