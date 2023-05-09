4dad75e017f9817b69d57e4170c8fb055f6b8732
Cory Fitz <coryalanfitz@gmail.com>
2023-05-09 21:10:57 +0100

    refactor

--- backend/recommender/views.py
+++ backend/recommender/views.py
@@ -6,20 +6,20 @@ from .serializers import RecommenderSerializer
 
 class RecommenderView(views.APIView):
 
-    def get_response(self):
+    def get_response(self, preferences):
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                     {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": "Please recommend a movie for me to watch. I prefer serious movies, I prefer thinking about the future, and I'm ok watching movies with subtitles"}
+                    {"role": "user", "content": f"Please recommend a movie for me to watch. {preferences}"}
                 ]
         )
 
         return {'response': response['choices'][0]['message']['content']}
 
     def get(self, request):
-        yourdata= self.get_response()
+        preferences = "I prefer serious movies, I prefer thinking about the future, and I'm ok watching movies with subtitles"
+        yourdata= self.get_response(preferences)
         results = RecommenderSerializer(yourdata).data
         return Response(results)
-    
     
