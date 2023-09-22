0c02e4fe5381cd4a6ac438ea9017e2331c2120cf
Cory Fitz <coryalanfitz@gmail.com>
2023-09-22 14:39:07 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
